9381beafdda170c49fbd69673b44b439192cd038
megaerni <165853906+megaerni@users.noreply.github.com>
2025-10-14 03:58:08 +0000

    Some work 10.13

--- BusSchedule.py
+++ BusSchedule.py
@@ -1,7 +1,7 @@
 #BusSchedule.py
-#Name:
-#Date:
-#Assignment:
+#Name: Meg Aerni
+#Date: 10.13.2025
+#Assignment: Homework 2
 
 import datetime
 from selenium import webdriver
@@ -38,11 +38,56 @@ def loadTestPage():
 
   return contents
 
+validTimes = []
+
+#function that analyzes a word to determine whether that word is a time
+def isItaTime(word):
+  word = word.strip()
+  if len(word) < 5:
+    return False
+  if ":" not in word:
+    return False
+  if "PM" not in word and "AM" not in word:
+    return False
+  else:
+    validTimes.append(word)
+
+#function that filters through all words in the page to determine which are times
+def retrieveTimes():
+  c1 = loadTestPage() #loads the test page
+  wordContents = c1.split()
+  for word in wordContents:
+    isItaTime(word)
+  return validTimes
+
+#this function calculates the given time in minutes
+def getCurrentTime(time):
+  currentTime = 0
+  time = time.replace("AM", "").replace("PM", "").strip()
+  parts = time.split(":")
+  hour = int(parts[0])
+  minute = int(parts[1])
+  currentTime = currentTime + (hour * 60)
+  currentTime = currentTime + (minute)
+  print(currentTime)
+
+#def getHours(time):
 
 def main():
-  url = "https://myride.ometro.com/Schedule?stopCode=2269&routeNumber=11&directionName=EAST"
-  #c1 = loadURL(url) #loads the web page
-  c1 = loadTestPage() #loads the test page
+  busStopNum = "2269"
+  routeNum = "11"
+  direction = "EAST"
+  url = "https://myride.ometro.com/Schedule?stopCode=" +busStopNum +"&routeNumber=" +routeNum +"&directionName=" +direction
+  c1 = loadURL(url) #loads the web page
   print(c1)
+  r1 = retrieveTimes()
+  now = datetime.datetime.now()
+  currentHour = (now.hour - 5) % 12
+  currentMinute = now.minute
+  print (currentHour, currentMinute)
+  print(validTimes)
+  #r2 = getCurrentTime(validTimes)
+  #print(r2)
+  
 
 main()
